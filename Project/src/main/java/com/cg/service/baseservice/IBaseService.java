e7b3853aa15ecb56ab52497a321c866acdf8c3f3
Quang Huy <2hnono30@gmail.com>
2023-04-18 10:51:04 +0700

    no message

--- Project/src/main/java/com/cg/service/baseservice/IBaseService.java
+++ Project/src/main/java/com/cg/service/baseservice/IBaseService.java
@@ -1,16 +1,10 @@
-package com.cg.service.baseservice;
+package com.cg.service.baseService;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
-
-public interface IBaseService<T, S, R> {
+public interface IBaseService<T, S> {
     Page<T> getAllAndSearch(S searchRequest,Pageable pageable);
 
-    Optional<R> findById(Long id);
-
-    T create(R r);
-
-    Optional<R> findByName(String name);
+    
 }
