2077eaae06e6a37d03e19dbe40690f137e0071e0
YenTran <yentran71313@gmail.com>
2023-04-24 16:04:14 +0700

    remove config

--- Project/src/main/java/com/cg/security/SecurityConfig.java
+++ Project/src/main/java/com/cg/security/SecurityConfig.java
@@ -66,7 +66,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/api/auth/login",
                         "/api/auth/register",
                         "/login",
-                        "/logout"
+                        "/logout",
+                        "/api/images/**",
+                        "/api/products/**",
+                        "/api/categories/**",
+                        "/api/brands/**"
                 ).permitAll()
                 .antMatchers("/transfers", "/histories/**").hasAnyAuthority("ADMIN")
                 .antMatchers("/resources/**", "/assets/**").permitAll()
