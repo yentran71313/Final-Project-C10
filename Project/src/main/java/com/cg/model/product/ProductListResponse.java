6c254cbf2d389f6ceb57a39b728750382b94357f
YenTran <yentran71313@gmail.com>
2023-05-17 10:19:00 +0700

    fix conflit

--- Project/src/main/java/com/cg/model/product/ProductListResponse.java
+++ Project/src/main/java/com/cg/model/product/ProductListResponse.java
@@ -4,12 +4,10 @@ import com.cg.model.Image;
 import lombok.*;
 
 import java.math.BigDecimal;
-<<<<<<< HEAD
+
 import java.util.List;
-=======
-import java.util.Arrays;
->>>>>>> 4948ac4 (Huy_dev Update Brand Backend)
-import java.util.Map;
+
+
 import java.util.stream.Collectors;
 
 
@@ -29,15 +27,14 @@ public class ProductListResponse {
     private String avatar;
     private Long avatarId;
     private String warranty;
-<<<<<<< HEAD
+
     private List<String> images;
     private List<Long> imageIds;
 
     private String description;
-=======
 
-    private Map<Long,String> images;
->>>>>>> 4948ac4 (Huy_dev Update Brand Backend)
+
+
 
     public ProductListResponse(Long id, String name, BigDecimal price, BigDecimal marketPrice, String nameCategory, String nameBrand, String warranty) {
         this.id = id;
@@ -50,15 +47,5 @@ public class ProductListResponse {
 
     }
 
-    public ProductListResponse(Product product){
-        this.id = product.getId();
-        this.name = product.getName();
-        this.price = product.getPrice();
-        this.marketPrice = product.getMarketPrice();
-        this.nameCategory = product.getCategory().getName();
-        this.nameBrand = product.getBrand().getName();
-        this.warranty = product.getWarranty();
-        this.images = product.getImages().stream().collect(Collectors.toMap(Image::getId,image -> image.getFileUrl()));
-    }
-
+    
 }
